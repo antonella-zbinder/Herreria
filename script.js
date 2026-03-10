4cd450f1e070b3e430ed37a7a011b823304a70f4
Antonella <163878448+antonella-zbinder@users.noreply.github.com>
2026-03-10 11:32:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Inicializar Swiper
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -9,31 +8,25 @@ const swiper = new Swiper(".mySwiper", {
     breakpoints: { 1024: { slidesPerView: 2 } }
 });
 
-// Función Antes/Después
 function toggleComparar(event, elemento) {
     const despues = elemento.querySelector('.despues');
-    // Cambia la opacidad solo si el clic no fue en el video
     const video = elemento.querySelector('video');
 
-    // Verifica si el clic fue en el video o en el botón de reproducción
     if (event.target !== video) {
         despues.style.opacity = (despues.style.opacity === "1") ? "0" : "1";
     }
 }
 
-// Agregar eventos a los comparadores
 document.querySelectorAll('.comparador').forEach(comparador => {
     comparador.addEventListener('click', (event) => toggleComparar(event, comparador));
 });
 
-// Agregar un evento al video para evitar que el clic se propague
 document.querySelectorAll('.video-container video').forEach(video => {
     video.addEventListener('click', function (event) {
-        event.stopPropagation(); // Evita que el clic se propague al comparador
+        event.stopPropagation(); 
     });
 });
 
-// Efecto Hover PC
 document.querySelectorAll('.comparador').forEach(item => {
     item.addEventListener('mouseenter', () => item.querySelector('.despues').style.opacity = "1");
     item.addEventListener('mouseleave', () => item.querySelector('.despues').style.opacity = "0");
@@ -45,13 +38,11 @@ let currentIndex = 0;
 
 carouselButtons.forEach(button => {
     button.addEventListener('click', () => {
-        // Obtenemos el ancho exacto y la separación dinámicamente desde el CSS
         const card = document.querySelector('.polaroid-card');
         const cardWidth = card.offsetWidth;
         const style = window.getComputedStyle(carouselTrack);
         const gap = parseFloat(style.gap) || 0;
         
-        // El paso exacto que se tiene que mover el track
         const step = cardWidth + gap;
         const maxIndex = carouselTrack.children.length - 1;
 
@@ -64,3 +55,4 @@ carouselButtons.forEach(button => {
         carouselTrack.style.transform = `translateX(-${currentIndex * step}px)`;
     });
 });
+
